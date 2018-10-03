cf51ae865e7d16356f48db69b4597bed2010bd27
aotolbert@gmail.com <aotolbert@gmail.com>
2018-10-02 20:42:28 -0400

    added clear button

--- server.js
+++ server.js
@@ -58,6 +58,44 @@ app.post("/survey/add", function(req, res) {
     res.json(newPerson);
   });
 
+app.post("/survey/clear", function(req, res) {
+    people = [
+        {
+            name: "Mick Jagger",
+            image: "https://upload.wikimedia.org/wikipedia/commons/6/66/Mick-Jagger-1965b.jpg",
+            scores: [5, 1, 3, 1, 4, 1, 2, 4, 2, 1]
+        },
+        {
+            name: "Paul McCartney",
+            image: "https://e.snmc.io/i/300/s/684f84f7fb8ecebe866b2f9496d4350f/1188523",
+            scores: [5, 1, 3, 1, 4, 1, 2, 4, 2, 1]
+        },
+        {
+            name: "Carlos Santana",
+            image: "http://forguitarplayersonly.com/wp-content/uploads/Santana.jpg",
+            scores: [5, 3, 1, 4, 5, 1, 4, 5, 1, 4]
+        },
+        {
+            name: "Kendrick Lamar",
+            image: "http://www.godisinthetvzine.co.uk/wp-content/uploads/2017/10/tde-holiday-collection-2016-kendrick-lamar.jpg",
+            scores: [1, 1, 4, 2, 3, 5, 5, 2, 5, 5]
+        },
+        {
+            name: "Ariana Grande",
+            image: "https://www.billboard.com/files/styles/article_main_image/public/media/ariana-grande-press-2015-billboard-650-d.jpg",
+            scores: [1, 1, 5, 1, 2, 5, 3, 4, 2, 5]
+        },
+        {
+            name: "Stevie Nicks",
+            image: "https://i.ebayimg.com/images/g/gZEAAOSwTM5YyHZy/s-l300.jpg",
+            scores: [5, 2, 1, 2, 5, 1, 1, 5, 1, 2]
+        }
+    ];
+    
+    // alert("Successfully placed into the iHarmony Database!!");
+
+});
+
 
   
 
